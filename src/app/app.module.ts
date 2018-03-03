c13c071261f29ae396964f1fe0272574c7e90246
Mark Schafers <mark_schafers@hotmail.com>
2018-03-03 11:55:22 +0800

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/home/about/about.component';
+import { ServicesComponent } from './components/home/services/services.component';
+import { SliderComponent } from './components/home/slider/slider.component';
+import { ContactComponent } from './components/home/contact/contact.component';
+import { FooterComponent } from './components/home/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { EditShowcaseComponent } from './components/edit-showcase/edit-showcase.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServicesComponent,
+    SliderComponent,
+    ContactComponent,
+    FooterComponent,
+    NavbarComponent,
+    LoginComponent,
+    GalleryComponent,
+    EditShowcaseComponent
   ],
   imports: [
     BrowserModule
