2472648dd515c0c13e7438e507a3b52c72434074
Mark Schafers <mark_schafers@hotmail.com>
2018-03-03 14:46:53 +0800

    added scrollspy and scrollfire features

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 declare let $: any;
+declare let Materialize: any;
 
 @Component({
   selector: 'app-navbar',
@@ -11,5 +12,42 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit() {
     $('.button-collapse').sideNav();
+
+    $(document).ready(function() {
+      $('.scrollspy').scrollSpy();
+    });
+
+    const options = [
+      {
+        selector: '.about-selector',
+        offset: 150,
+        callback: function(el) {
+          Materialize.showStaggeredList($(el));
+        }
+      },
+      {
+        selector: '.service-selector',
+        offset: 155,
+        callback: function(el) {
+          Materialize.showStaggeredList($(el));
+        }
+      },
+      {
+        selector: '.contact-selector',
+        offset: 265,
+        callback: function(el) {
+          Materialize.showStaggeredList($(el));
+        }
+      },
+      {
+        selector: '.navbar-fixed',
+        offset: 1920,
+        callback: function() {
+          $('nav').removeClass('transparent');
+          $('nav').addClass('blue-grey darken-3');
+        }
+      }
+    ];
+    Materialize.scrollFire(options);
   }
 }
