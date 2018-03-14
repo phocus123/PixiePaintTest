2dfb47fb63087f98a3a3509ac4fd65d7820c7cea
Mark Schafers <mark_schafers@hotmail.com>
2018-03-14 17:44:16 +0800

    fixed problem where login routes werent being found when hosting and made some other changes

---
+++ src/app/components/gallery/insta-feed/insta-feed.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+declare let $: any;
+
+@Component({
+  selector: 'app-insta-feed',
+  templateUrl: './insta-feed.component.html',
+  styleUrls: ['./insta-feed.component.css']
+})
+export class InstaFeedComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    $(document).ready(function() {
+      // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+      $('.modal').modal();
+      $('#modal1').modal('open');
+    });
+  }
+}
