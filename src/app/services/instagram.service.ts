4e99dace9b9b11ab998a3d349fd35546e2fb47e6
Mark Schafers <mark_schafers@hotmail.com>
2018-03-18 15:13:18 +0800

    added pixie paint instagram to the gallery

--- src/app/services/instagram.service.ts
+++ src/app/services/instagram.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/observable';
 
 @Injectable()
 export class InstagramService {
-  instagramUrl = 'https://api.instagram.com/v1/users/193800954/media/recent/?count=99&access_token=193800954.d30613d.9d8c8abca0ac44a2b7acc814307353a5';
+  instagramUrl = 'https://api.instagram.com/v1/users/4089301952/media/recent/?count=99&access_token=4089301952.d30613d.e0ca2a66ca9c47b1a00acb437e38d050';
 
   constructor(private http: HttpClient) {}
 
